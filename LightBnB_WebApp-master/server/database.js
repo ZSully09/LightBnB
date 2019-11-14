dcdbbb86f9ef71b439c9a39e7dc57e5b35b3a0e1
ZSully09 <zsullivan93@gmail.com>
2019-11-14 23:07:42 +0000

    refactored getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,7 +74,7 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  console.log(user);
+  // console.log(user);
   return pool
     .query(
       `
@@ -104,7 +104,29 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // console.log(guest_id);
+  return pool
+    .query(
+      `
+      SELECT properties.*, reservations.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id 
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date DESC
+      LIMIT $2;
+      `,
+      [guest_id, limit]
+    )
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  // return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
 
